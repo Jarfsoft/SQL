834df03aafa8d6b229b2426eef2d64270ab7bff6
Juan Raudales <juanraudales@unitec.edu>
2020-12-05 06:00:57 -0600

    The JOIN operation lesson

--- SQL_Zoo.sql
+++ SQL_Zoo.sql
@@ -246,3 +246,33 @@ SELECT continent FROM world x
   WHERE (SELECT SUM(population) FROM world WHERE continent = x.continent) >= 100000000
  GROUP BY continent
 
+"The JOIN operation"
+
+SELECT matchid, player FROM goal 
+  WHERE teamid = 'GER'
+
+SELECT id,stadium,team1,team2
+  FROM game
+ WHERE id = 1012
+
+SELECT player, teamid, stadium, mdate
+  FROM game JOIN goal ON (id=matchid)
+ WHERE teamid = 'GER'
+
+SELECT team1, team2, player
+  FROM game JOIN goal ON (id=matchid)
+ WHERE player LIKE 'Mario%'
+
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON teamid = id
+ WHERE gtime<=10
+
+SELECT mdate, teamname
+   FROM game JOIN eteam ON team1 = eteam.id
+  WHERE coach = 'Fernando Santos'
+
+SELECT player
+  FROM goal JOIN game ON matchid = game.id
+ WHERE stadium = 'National Stadium, Warsaw'
+
+ 
